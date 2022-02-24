dc90beb8a708ba66e142eeb3f340d32f660c1b31
cuong-holvi <cnguyen@holvi.com>
2022-02-24 22:47:46 +0200

    Add WIP analysis

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import pandas
+import pandas as pd
+import pandasql as ps
 import sqlite3
 
 conn = sqlite3.connect("SQLite.db")
 cur = conn.cursor()
-csvfile = "feed_station_mapping.csv"
-table_name = "feed_station_mapping"
+csvfiles = ["feed_station_mapping (1).csv", "consumptions (1).csv"]
+table_name = ["feed_station_mapping", "consumptions"]
 
-df = pandas.read_csv(csvfile)
-df.to_sql(table_name, conn, if_exists='fail', index=False)
+for csv, table in zip(csvfiles, table_name):
+    df = pd.read_csv(csv)
+    df.to_sql(table, conn, if_exists='fail', index=False)
